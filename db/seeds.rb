90ebf535eb3337171fda3252a9eb7e867ec61d7b
CHRIS ADAMO <adamomeister@gmail.com>
2018-11-18 17:21:38 -0600

    add db seeds file for inputting data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+inputData = [
+  "3004 - Termite Treatments",  #revenue_name
+  2018,                       #year
+  28331.00,                           #january
+  20297.00,                           #february
+  32019.00,                           #march
+  23393.00,                           #april
+  19196.00,                           #may
+  17513.00,                           #june
+  22095.00,                           #july
+  27954.00,                           #august
+  20726.00,                           #september
+  # ,                           #october
+  # ,                           #november
+  # ,                           #december
+  # ,                           #total  
+  ]
+
+
+profit_loss = ProfitLoss.new(
+  revenue_name: inputData[0],
+  year: inputData[1],
+  january: inputData[2],
+  february: inputData[3],
+  march: inputData[4],
+  april: inputData[5],
+  may: inputData[6],
+  june: inputData[7],
+  july: inputData[8],
+  august: inputData[9],
+  september: inputData[10],
+  october: inputData[11],
+  november: inputData[12],
+  december: inputData[13],
+  total: inputData[14],
+  )
+
+profit_loss.save
