c5f5fa3ab1523fb926ed7991f8a9ce204f9a4c22
CHRIS ADAMO <adamomeister@gmail.com>
2018-12-17 21:33:27 -0600

    create post action to read in file from frontend

--- app/controllers/api/profit_losses_controller.rb
+++ app/controllers/api/profit_losses_controller.rb
@@ -54,4 +54,61 @@ class Api::ProfitLossesController < ApplicationController
     end
     render 'index_profit_loss_query.json.jbuilder'
   end
+
+  def update
+    uploaded_io = params[:file]
+    File.open(Rails.root.join('public', uploaded_io.original_filename), 'wb') do |file|
+      file.write(uploaded_io.read)
+    end
+    # Load the excel file in .xls format
+    input_file = Spreadsheet.open Rails.root.join('public', uploaded_io.original_filename)
+
+    # Load first sheet from excel file
+    input_sheet = input_file.worksheet 2
+    
+    input_row = input_sheet.row(1)[0]
+    p input_row
+
+    # j = 1
+    # while j <= 56
+    #   # Set array to a specific row in the sheet
+    #   input_row = input_sheet.row(j)
+
+    #   # First value is the row title (string)
+    #   input_data = [input_row[0]]
+
+    #   i = 1
+
+    #   while i <= 12
+    #     if input_row[i] == nil
+    #       input_data << 0
+    #     else
+    #       input_data << input_row[i]
+    #     end
+    #     i += 1
+    #   end
+
+    #   j += 1
+    #   # pp input_data
+    #   profit_loss = ProfitLoss.new(
+    #     revenue_name: input_data[0],
+    #     year: 2017,
+    #     january: input_data[1],
+    #     february: input_data[2],
+    #     march: input_data[3],
+    #     april: input_data[4],
+    #     may: input_data[5],
+    #     june: input_data[6],
+    #     july: input_data[7],
+    #     august: input_data[8],
+    #     september: input_data[9],
+    #     october: input_data[10],
+    #     november: input_data[11],
+    #     december: input_data[12],
+    #     total: 0,
+    #   )
+
+    #   profit_loss.save
+    # end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   namespace :api do
     get '/profit_losses' => 'profit_losses#index'
     get '/profit_losses/profit_loss_query' => 'profit_losses#index_profit_loss_query'
+    post '/profit_losses/file_upload' => 'profit_losses#update'
   end 
 end
